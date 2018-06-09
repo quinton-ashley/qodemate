4a6f9efdab3941ae2c035a78ee79d4522ff7e5d5
Quinton Ashley <qashto@gmail.com>
2018-06-09 02:55:03 -0400

    0.0.26

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const isDev = fs.existsSync(__dirname + '/../qodemate-core');
-
-if (isDev) {
-	require('../qodemate-core')();
-} else {
-	require('qodemate-core')();
-}
+console.log('starting Qodemate!');
+require('sidecar');
+require('qodemate-desktop')();
